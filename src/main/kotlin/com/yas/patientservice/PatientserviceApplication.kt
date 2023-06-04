845fce0921eddb9148fe5939a6db0f912e7063f4
YThilantha <yasitha.thilantha@cambio.lk>
2023-06-04 15:00:49 +0530

    init commmit

---
+++ src/main/kotlin/com/yas/patientservice/PatientserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.yas.patientservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatientserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PatientserviceApplication>(*args)
+}
